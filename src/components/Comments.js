281e71f8b82a81a6a7eaa289185a21b2c67da741
Leo Belyi <leonid@ac93.org>
2019-01-14 15:00:26 -0500

    Automatically fetch messages as you go up

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -7,29 +7,14 @@ type Props = {
 };
 
 class Comments extends Component<Props> {
-    scrollToBottom() {
-        if (!this.scrollView) return;
-        this.scrollView.scrollToBottom();
-    }
-
-    scrollToTop() {
-        if (!this.scrollView) return;
-        this.scrollView.scrollToTop();
-    }
-
-    handleScroll = ({ scrollTop, scrollBottom }) => {
-        console.log('scrollTop', scrollTop);
-        console.log('scrollBottom', scrollBottom);
-    };
-
     render() {
         return (
             <div>
-                {
-                    this.props.comments.slice(0).reverse().map((comment, index) => {
-                        return <Comment key={index} comment={comment} />
-                    })
-                }
+            {
+                this.props.comments.slice(0).reverse().map((comment, index) => {
+                    return <Comment key={index} comment={comment} />
+                })
+            }
             </div>
         );
     }
