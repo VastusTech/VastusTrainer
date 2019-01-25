9251740614b53dda3ec3e5e5cda596a36115be8b
Leo Belyi <leonid@ac93.org>
2019-01-24 20:25:53 -0500

    Fetch user attributes before app starts up!

--- src/redux_helpers/actions/userActions.js
+++ src/redux_helpers/actions/userActions.js
@@ -49,86 +49,9 @@ export function fetchUserAttributes(variablesList, dataHandler) {
                 dispatch(setIsNotLoading());
                 if (dataHandler) { dataHandler(getStore().user);}
             })(dispatch, getStore);
-            // console.log("Filtering out results for fetch!");
-            // const userKeyList = Object.keys(user);
-            // // console.log("Originally asked for variablesList = " + JSON.stringify(variablesList));
-            // // console.log("UserKeyList = " + JSON.stringify(userKeyList));
-            // const filterVariablesList = variablesList.filter((v) => {
-            //     return !userKeyList.includes(v)
-            // });
-            // // console.log("Final filtered list is = " + JSON.stringify(filterVariablesList));
-            // overwriteFetchUserAttributes(user.id, filterVariablesList, dataHandler, dispatch, getStore);
         }
     }
 }
-
-// function overwriteFetchUserAttributes(id, variablesList, dataHandler, dispatch, getStore) {
-//     dispatch(setIsLoading());
-//     if (variablesList.length > 0) {
-//         const pictureIndex = variablesList.indexOf("profilePicture");
-//         if (pictureIndex !== -1) {
-//             variablesList.splice(pictureIndex, 1);
-//         }
-//         QL.getClient(id, variablesList, (data) => {
-//             if (pictureIndex !== -1) {
-//                 if (data.profileImagePath) {
-//                     Storage.get(data.profileImagePath).then((url) => {
-//                         data = {
-//                             ...data,
-//                             profilePicture: url
-//                         };
-//                         dispatch(setUser(data));
-//                         dispatch(setIsNotLoading());
-//                         if (dataHandler) { dataHandler(getStore().user);}
-//                     }, (error) => {
-//                         console.log("Failed to get profile image");
-//                         console.log(error);
-//                         dispatch(setUser(data));
-//                         dispatch(setIsNotLoading());
-//                         if (dataHandler) { dataHandler(getStore().user);}
-//                     });
-//                 }
-//                 else {
-//                     // Put the default image there
-//                     data = {
-//                         ...data,
-//                         profilePicture: defaultProfilePicture
-//                     };
-//                     dispatch(setUser(data));
-//                     dispatch(setIsNotLoading());
-//                     if (dataHandler) { dataHandler(getStore().user);}
-//                 }
-//             }
-//             else {
-//                 dispatch(setUser(data));
-//                 dispatch(setIsNotLoading());
-//                 if (dataHandler) { dataHandler(getStore().user);}
-//             }
-//         }, (error) => {
-//             console.log(JSON.stringify(error));
-//             dispatch(setError(error));
-//             dispatch(setIsNotLoading());
-//         });
-//     }
-//     else {
-//         dispatch(setIsNotLoading());
-//         if (dataHandler) { dataHandler(getStore().user);}
-//     }
-// }
-
-// export function fetchUser(username, successHandler, failureHandler) {
-//     return (dispatch) => {
-//         dispatch(setIsLoading());
-//         QL.getClientByUsername(username, ["id", "username"], (data) => {
-//             dispatch(setUser(data));
-//             dispatch(setIsNotLoading());
-//         }, (error) => {
-//             console.log(JSON.stringify(error));
-//             dispatch(setError(error));
-//             dispatch(setIsNotLoading());
-//         });
-//     }
-// }
 
 export function clearUser() {
     return {
